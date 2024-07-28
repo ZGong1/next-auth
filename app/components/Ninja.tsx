d19b5b91c7a15adac5a5216209eef326704e1e1f
Eric <ericpeterson@tamu.edu>
2024-07-28 18:08:45 -0500

    Ninja component w/o buttons + icons

--- app/components/Ninja.tsx
+++ app/components/Ninja.tsx
@@ -4,7 +4,37 @@ import { NinjaType } from "../types/pbdb";
 
 const Ninja = ( { ninjaData }: {ninjaData: NinjaType}) => {
   return (
-    ninjaData.name
+    <div className="outerNinja">
+
+      <div className={`innerLeft ${ninjaData.belt}`}>
+        <img className='image' src={`icons/IconSquare${ninjaData.imgNum}.png`}/>
+        <p className='ninjaName'>{ninjaData.name}</p>
+      </div>
+
+      <div className='ninjaData'>
+
+        <p>Ninja Bucks: {ninjaData.bucks}</p> <br/>
+
+        Last Level Up: {ninjaData.llu}<br/>
+
+        {ninjaData.ice && <p className='emoji'>🍦</p>}
+        {!ninjaData.ice && <p className='emoji'>❌</p>}
+
+        <div>
+          <button className='ninja-button' >Level Up</button>
+          <button className='ninja-button' >Belt Up</button>
+          <button className='ninja-button' >Belt Down</button>
+          <button className='ninja-button' >Ice Cream</button>
+        </div>
+
+      </div>
+
+      <div>
+        <button className="editButton">✏️</button>
+        <button className="editButton">❌</button>
+      </div>
+
+    </div>
   );
 }
  
