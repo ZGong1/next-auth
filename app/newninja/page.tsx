c28fbbb29468ab30ef76383533db0917204a2d75
Eric <ericpeterson@tamu.edu>
2024-07-29 20:35:35 -0500

    functional but ugly add ninja page

--- app/newninja/page.tsx
+++ app/newninja/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
+import { useState } from "react";
+import { createNinja } from "../actions/dbActions";
+
 const Home = () => {
+
+  const [name, setName] = useState("")
+  const [bucks, setBucks] = useState(0)
+
+  const onSubmit = () => {
+    createNinja(name, bucks)
+  }
+
   return (
     <div>
-      Hello empty new ninja page
+      <h1>Enter New Ninja's Info</h1>
+      Name: <br/><input value={name} onChange={e => setName(e.target.value)} /><br/>
+      Ninja Bucks: <br/><input type="number" value={bucks} onChange={e => setBucks(Number(e.target.value))}/><br/><br/>
+      <button onClick={onSubmit}>Add</button>
     </div>
   );
 }
