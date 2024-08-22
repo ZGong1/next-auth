0893c21162b763e3b121433030f7c7166d148408
Eric <ericpeterson@tamu.edu>
2024-08-22 16:35:14 -0500

    search bar functional

--- app/components/NinjaList.tsx
+++ app/components/NinjaList.tsx
@@ -1,15 +1,22 @@
 "use client"
 
+import { useState } from "react";
 import { NinjaListType } from "../types/pbdb";
 import Ninja from "./Ninja";
 
 const NinjaList = ( { ninjaList }: { ninjaList: NinjaListType } ) => {
+
+  const [searchString, setSearchString] = useState("")
+  const toShow = ninjaList.filter(item => {
+    return item.name.toUpperCase().includes(searchString.toUpperCase())
+  })
+
   return (<>
     <div className="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 shadow my-4 bg-white">
-      Search: <input type="text"/>
+      Search: <input type="text" value={searchString} onChange={e => setSearchString(e.target.value)}/>
     </div>
 
-    {ninjaList.map(ninjaData => {
+    {toShow.map(ninjaData => {
       return <Ninja key={ninjaData.id} ninjaData={ninjaData}/>
     })}
   </>);
